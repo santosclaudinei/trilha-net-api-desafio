000946a450a5219957368754f3e078b24d77595c
Claudinei Santos <claudinei.santos@warren.com.br>
2022-12-27 00:10:44 -0300

    refactor: remocao de usings e utilizacao de padrao net 6.0

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -5,10 +5,7 @@ namespace TrilhaApiDesafio.Context
 {
     public class OrganizadorContext : DbContext
     {
-        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
-        {
-            
-        }
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options) { }
 
         public DbSet<Tarefa> Tarefas { get; set; }
     }
